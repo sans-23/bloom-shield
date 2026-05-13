9dd9d28af0a8b0eafd84baad4be805ae49498693
sanskar <sanskarjainskj@gmail.com>
2026-05-13 19:24:53 +0530

    intialised spring boot application

---
+++ src/main/java/BloomShield/BloomShieldApplication.java
@@ -0,0 +1,13 @@
+package BloomShield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomShieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BloomShield/BloomShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package BloomShield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
