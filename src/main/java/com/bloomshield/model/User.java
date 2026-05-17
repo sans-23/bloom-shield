9bf4971ddd9bff34ed501f92d4925829382e2506
sanskar <sanskarjainskj@gmail.com>
2026-05-17 18:48:36 +0530

    fix: resolve Hibernate InstantiationException for User entity
    - Added Lombok's `@NoArgsConstructor` to `User.java` because Hibernate requires a default, no-argument constructor to instantiate entities when retrieving them from the database.

--- src/main/java/com/bloomshield/model/User.java
+++ src/main/java/com/bloomshield/model/User.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     
     @Id
